c955f0c02d92bcb914a79d100b9d1fd6ce681db4
ether-lord <omnipresent.ether@yandex.ru>
2025-02-02 12:03:28 +1000

    attrib bug fix

--- src/modules/resources.cpp
+++ src/modules/resources.cpp
@@ -58,13 +58,14 @@ TextureLoader::TextureLoader(flecs::world& world) {
   world.component<Texture>();
   world.component<TextureBuffer>();
 
+  stbi_set_flip_vertically_on_load(true);
+
   world.system<const Texture>("Texture Loader")
       .kind(flecs::OnLoad)
       .each([](flecs::entity e, const Texture& texture_data) {
         auto texture_path =
             ResourcesManager::get().GetPathToTexture(texture_data.name);
 
-        stbi_set_flip_vertically_on_load(true);
         int width, height, nr_channels;
         unsigned char* data =
             stbi_load(texture_path.c_str(), &width, &height, &nr_channels, 0);
@@ -99,8 +100,8 @@ Buffering::Buffering(flecs::world& world) {
   world.system<const Mesh>("Mesh Buffering System")
       .kind(flecs::OnLoad)
       .each([](flecs::entity e, const Mesh& mesh_data) {
-        auto vertices = mesh_data.vertices;
-        auto indices = mesh_data.indices;
+        const auto& vertices = mesh_data.vertices;
+        const auto& indices = mesh_data.indices;
 
         vector<float> vbo_data;
         for (const auto& v : vertices) {
@@ -114,21 +115,26 @@ Buffering::Buffering(flecs::world& world) {
         }
 
         unsigned int vao, vbo, ebo;
+
         glGenVertexArrays(1, &vao);
         glGenBuffers(1, &vbo);
         glGenBuffers(1, &ebo);
+
         glBindVertexArray(vao);
+
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vbo_data[0]) * vbo_data.size(),
                      &vbo_data[0], GL_STATIC_DRAW);
+
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER,
                      sizeof(indices[0]) * indices.size(), &indices[0],
                      GL_STATIC_DRAW);
+
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                               (void*)0);
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                               (void*)offsetof(Vertex, uv));
         glEnableVertexAttribArray(1);
 
